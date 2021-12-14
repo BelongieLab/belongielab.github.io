d6bc4b03570b4e51312a8f3e2352054fd5b1df8e
KMnP <mj493@cornell.edu>
2021-12-14 17:31:04 -0500

    clean cache

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# belongielab.github.io
-Lab Website
--- _site/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#bundle install
-
-# remove cached site and folders first for a clean rendering
-rm -rf Gemfile.lock
-rm -rf _site
-rm -rf .jekyll-cache
-
-bundle exec jekyll serve
